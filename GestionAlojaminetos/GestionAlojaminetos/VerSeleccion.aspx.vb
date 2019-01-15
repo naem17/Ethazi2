727a0760dd5a142a5e2849173ac5a869089a65f5
Ibon Alonso <ibonalofer@gmail.com>
2019-01-15 11:15:08 +0100

    IAF: Echa que la pagina pueda ser modular

--- GestionAlojaminetos/GestionAlojaminetos/VerSeleccion.aspx.vb
+++ GestionAlojaminetos/GestionAlojaminetos/VerSeleccion.aspx.vb
@@ -3,19 +3,52 @@ Public Class VerSeleccion
     Inherits System.Web.UI.Page
     Dim cnn1 As MySqlConnection = Site1.cnn1
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim firma As String = Request.QueryString().ToString()
+        Dim datos() As String = Split(Request.QueryString().ToString(), "-")
+
         If cnn1.State = ConnectionState.Closed Then
             cnn1.Open()
         End If
         Dim cmd1 = cnn1.CreateCommand()
-        cmd1.CommandText = "SELECT * FROM alojamientos WHERE FIRMA = @firma"
-        cmd1.Parameters.AddWithValue("@firma", firma)
+        datos(1) = datos(1).ToLower
+        Select Case datos(1)
+            Case "alojamientos"
+                cmd1.CommandText = "SELECT * FROM `@tabla` WHERE FIRMA = `@firma`"
+                cmd1.Parameters.AddWithValue("@firma", datos(0))
+                cmd1.Parameters.AddWithValue("@tabla", datos(1))
+            Case "categorias"
+                cmd1.CommandText = "SELECT * FROM `@tabla` WHERE CODIGO = `@cod`"
+                cmd1.Parameters.AddWithValue("@cod", datos(0))
+                cmd1.Parameters.AddWithValue("@tabla", datos(1))
+            Case "codigos_postales"
+                cmd1.CommandText = "SELECT * FROM `@tabla` WHERE ID = `@cod`"
+                cmd1.Parameters.AddWithValue("@cod", datos(0))
+                cmd1.Parameters.AddWithValue("@tabla", datos(1))
+            Case "municipios"
+                cmd1.CommandText = "SELECT * FROM @tabla WHERE INDICE = @cod"
+                cmd1.Parameters.AddWithValue("@cod", datos(0))
+                cmd1.Parameters.AddWithValue("@tabla", datos(1))
+            Case "categorias"
+                cmd1.CommandText = "SELECT * FROM @tabla WHERE CODIGO = @cod"
+                cmd1.Parameters.AddWithValue("@cod", datos(0))
+                cmd1.Parameters.AddWithValue("@tabla", datos(1))
+            Case "tipos"
+                cmd1.CommandText = "SELECT * FROM @tabla WHERE CODIGO = @cod"
+                cmd1.Parameters.AddWithValue("@cod", datos(0))
+                cmd1.Parameters.AddWithValue("@tabla", datos(1))
+            Case "tipos_euskera"
+                cmd1.CommandText = "SELECT * FROM @tabla WHERE CODIGO = @cod"
+                cmd1.Parameters.AddWithValue("@cod", datos(0))
+                cmd1.Parameters.AddWithValue("@tabla", datos(1))
+            Case Else
+                MsgBox("No se a creado select")
+        End Select
+
         Dim da1 As New MySqlDataAdapter
         da1.SelectCommand = cmd1
         Dim dt As New DataTable
         da1.Fill(dt)
         Me.GridView1.DataSource = dt
         Me.GridView1.DataBind()
-        Label1.Text = firma
+        Label1.Text = datos(0)
     End Sub
 End Class
